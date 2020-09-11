24b63203462b06166267c6478ae86a24ffd54639
sky <rana.akash0003@gmail.com>
2020-09-11 11:10:32 +0600

    Add repository

---
+++ src/views/app/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import { Provider } from 'react-redux'
+// import store from '../../redux/Store/store'
+
+import './app.scss'
+
+function App() {
+	return (
+		// <Provider store={store}>
+		<div className='app-container'>
+			<h1>Welcome react-setup frontend</h1>
+		</div>
+		// </Provider>
+	)
+}
+
+export default App
